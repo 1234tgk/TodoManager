82fb9be035e54fe35aa1c1ba5fc8686945334b0f
Taehyeon Kim <1234tgk@gmail.com>
2023-09-04 21:33:07 -0400

    add auth link on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,6 +7,23 @@ export default function Home() {
       <div className='min-h-screen flex items-center justify-center bg-gray-100'>
         <div className='bg-white p-8 rounded shadow-md w-96 text-center'>
           <h2 className='text-2xl mb-4'>Welcome!</h2>
+
+          <p className='mt-4 text-center'>
+            <Link
+              href='/auth/login'
+              className='text-blue-500 hover:text-blue-600'
+            >
+              Login
+            </Link>
+            {"  "}
+            or {"  "}
+            <Link
+              href='/auth/sign-up'
+              className='text-blue-500 hover:text-blue-600'
+            >
+              Sign Up
+            </Link>
+          </p>
         </div>
       </div>
     </>
